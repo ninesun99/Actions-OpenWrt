b310b12a413686e5aff22f15efc59f722d612e42
ninesun99 <ninesun_ninesun@outlook.com>
2026-03-24 20:09:35 +0800

    Modify default IP and set Argon theme in diy-part2.sh
    Updated default IP and theme settings in the OpenWrt DIY script.

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 1. 把 237 的默认 192.168.6.1 改成你喜欢的 192.168.8.1
+sed -i 's/192.168.6.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 2. 强行将默认的主题设为 Argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
