c9ad350f94347eff473837717ec1009e1244a601
ninesun99 <ninesun_ninesun@outlook.com>
2026-03-24 20:22:21 +0800

    Modify OpenWrt DIY script for IP and theme settings
    Updated default IP and theme in OpenWrt DIY script. Added backup and configuration merging steps.

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-# Description: OpenWrt DIY script part 2 (After Update feeds)
+# Description: OpenWrt DIY script part 2
 
-# 1. 把 237 的默认 192.168.6.1 改成你喜欢的 192.168.8.1
+# 暴力修改默认 IP 为 192.168.8.1
 sed -i 's/192.168.6.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
-# 2. 强行将默认的主题设为 Argon
+# 强行将默认的主题设为 Argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 核心防坑：融合 237 大佬官方闭源驱动底包
+mv .config .config.bak
+cp -f defconfig/mt7981-ax3000.config .config
+cat .config.bak >> .config
